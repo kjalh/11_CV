e263fc0c63914488ec48387e415ff6c449993e45
희승ACMY <heseung0895@naver.com>
2026-08-26 13:38:09 +0900

    aaa

--- 02_OpenCV/01_opencv.py
+++ 02_OpenCV/01_opencv.py
@@ -1,3 +1,22 @@
 """
+OpenCV(Open Source Computer Vision Library)
+OpenCV는 이미지(영상)을 처리하기 위한 대표적인 오픈소스 컴퓨터 비전 라이브러리
+
+- 이미지(영상) 읽기와 저장
+- 색상 공간 변환
+- 크기 변경, 회전, 자르기
+- 필터링과 경계선 검출
+- 이진화와 객체 분할
+- 특징점 검출
+- 카메라/동영상 처리
+- 머신러닝/딥러닝/모델과의 연동
+
+> OpenCV에서 읽은 이미지는 기본적으로 Numpy 배열로 다루기 때문에 Numpy 인덱싱, 슬라이싱, 배열 연산을 그대로 활용할 수 있음
+
+pip install opencv-python
 
 """
+
+import cv2
+
+print('현재 OpenCV 버전: ', cv2.__version__)
