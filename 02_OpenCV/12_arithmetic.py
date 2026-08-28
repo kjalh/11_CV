a129dadf706959802554df19e2aacf64f79995a7
이희승 <heseung0895@naver.com>
2026-08-28 12:13:10 +0900

    fff

--- 02_OpenCV/12_arithmetic.py
+++ 02_OpenCV/12_arithmetic.py
@@ -14,3 +14,20 @@ dst_blend = cv2.addWeighted(img1, 0.5, img2, 0.5, 0)
 # 3. 뺄셈. 음수가 되는 값은 0으로 제한
 dst_subtract = cv2.subtract(img1, img2)
 
+# 4. 절대 차이
+# |img1 - img2|
+# 두 영상에서 픽셀 값이 얼마나 다른지 확인할 때 사용
+dst_absdiff = cv2.absdiff(img1, img2)
+
+images = {"add": dst_add, "blend": dst_blend, "subtract": dst_subtract, "absdiff": dst_absdiff}
+
+plt.figure(figsize=(10,8))
+
+for i, (title, image) in enumerate(images.items(), start=1):
+    plt.subplot(2, 2, i)
+    plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+    plt.title(title)
+    plt.axis('off')
+plt.tight_layout()
+plt.show()
+
