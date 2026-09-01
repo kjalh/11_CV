ff368fb386dcb8617da084ad6fc092f0de1b91d9
이희승 <heseung0895@naver.com>
2026-09-01 09:46:40 +0900

    ㅇㅇ

--- 02_OpenCV/18_ROI_Selector.py
+++ 02_OpenCV/18_ROI_Selector.py
@@ -21,6 +21,27 @@ def on_mouse(event, x, y, flags, param):
         cv2.rectangle(preview, (x1, y1),(x2, y2), color, 2)
         cv2.imshow("img", preview)
 
+    elif event == cv2.EVENT_LBUTTONUP and is_dragging:
+        is_dragging = False
+        x1 = min(start_x, x)
+        y1 = min(start_y, y)
+        x2 = max(start_x, x)
+        y2 = max(start_y, y)
+
+        w = x2 - x1
+        h = y2 - y1
+
+        if w < 0 or h <= 0:
+            cv2.imshow("img", img)
+            print("영역이 설정되지 않았습니다.")
+            return
+        roi = img[y1:y2, x1:x2]
+        selected = img.copy()
+        cv2.rectangle(selected, (x1, y1), (x2, y2), color, 2)
+        cv2.imshow("img", selected)
+        cv2.imshow("roi", roi)
+        print(f"ROI 위치: x = {x1}, y = {y1}, w = {w}, h = {h}")
+
 img = cv2.imread("./images/sun.jpg")
 
 cv2.imshow("img", img)
---
+++ 02_OpenCV/19_binaruzation.py
@@ -0,0 +1,45 @@
+import cv2
+import matplotlib.pyplot as plt
+
+"""
+이진화
+픽셀 값을 두 그룹으로 나누는 영상 처리 기법
+
+일반적인 8비트 이진 영상 > 검정(0), 흰색(255)
+
+OCR, 문서 스캔, 윤곽선 검출, 객체 분리 등의 전처리에서 자주 사용됨
+"""
+
+img = cv2.imread("./images/cells.png", cv2.IMREAD_GRAYSCALE)
+img_color = cv2.imread("./images/cells.png")
+
+hist = cv2.calcHist([img], [0], None, [256], [0, 256])
+
+# 픽셀값 > threshold : 최대값
+# 픽셀값 <= threshold : 0
+threshold1, dst1 = cv2.threshold(img, 100, 255, cv2.THRESH_BINARY)  # 100보다 크면 싹다 255로
+print("임계값 1: ", threshold1)
+
+cv2.imshow("original", img)
+cv2.imshow("threshold 100", dst1)
+cv2.imshow("color", img_color)
+
+
+threshold2, dst2 = cv2.threshold(img, 210, 255, cv2.THRESH_BINARY)
+print("임계값 2: ", threshold2)
+cv2.imshow("threshold 255", dst2)
+
+"""
+Otsu 자동 임계값
+THRESH_OTSU를 사용하면 임계값을 사람이 직접 정하지 않고 영상의 히스토그램을 이용해 OpenCV가 임계값을 자동으로 선택
+"""
+
+plt.plot(hist)
+plt.title("Grayscale Histogram")
+plt.xlabel("Pixel Value")
+plt.ylabel("Count")
+plt.xlim([0, 256])
+plt.show()
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
