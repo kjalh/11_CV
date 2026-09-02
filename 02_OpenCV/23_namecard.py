7d326a79e7d1d54642809e3748d4cae43119fa02
이희승 <heseung0895@naver.com>
2026-09-02 10:23:55 +0900

    sss

--- 02_OpenCV/23_namecard.py
+++ 02_OpenCV/23_namecard.py
@@ -1,39 +1,148 @@
 import cv2
 import numpy as np
 
+
+def draw_roi(image, corners):
+    preview = image.copy()
+    point_color = (192, 192, 255)
+    line_color = (128, 128, 255)
+
+    for pt in corners:
+        cv2.circle(preview, tuple(pt.astype(int)), 12, point_color, -1)
+
+    for i in range(4):
+        pt1 = tuple(corners[i].astype(int))
+        pt2 = tuple(corners[(i+1) % 4].astype(int))
+        cv2.line(preview, pt1, pt2, line_color, 2)
+
+    return preview
+
+
+
+def on_mouse(event, x, y, flags, param):
+    global src_quad, drag_src
+
+    if event == cv2.EVENT_LBUTTONDOWN:
+        for i in range(4):
+            distance = cv2.norm(src_quad[i] - np.array([x, y], dtype=np.float32)) # 두 점의 직선거리를 구하는 함수
+
+            if distance < 20:
+                drag_src[i] = True
+                break
+
+    elif event == cv2.EVENT_MOUSEMOVE:
+        for i in range(4):
+            if drag_src[i]:
+                new_x = np.clip(x, 0, w - 1)
+                new_y = np.clip(y, 0, h - 1)
+                src_quad[i] = (new_x, new_y)
+                preview = draw_roi(img, src_quad)
+                cv2.imshow('img', preview)
+                break
+    elif event == cv2.EVENT_LBUTTONUP:
+        drag_src = [False, False, False, False]
+
+
 img = cv2.imread("./images/namecard.jpg")
 
-# 변환된 크기를 이렇게 할거다
-dst_w = 600
-dst_h = 400
+h, w = img.shape[:2]
 
-# 네 점의 순서는 왼쪽 위 > 오른쪽 위 > 오른쪽 아래 > 왼쪽 아래
+dst_h = 500
+dst_w = round(dst_h * 297 / 210)
+
+# 왼 위, 왼 아, 오 아, 오 위
 src_quad = np.array([
-    [16, 16],    
-    [700, 16],   
-    [700, 843],  
-    [16, 843]     
+    [30, 30],
+    [30, h - 30],
+    [w - 30, h - 30],
+    [w - 30, 30]
 ], dtype=np.float32)
 
 dst_quad = np.array([
     [0, 0],
-    [dst_w - 1, 0],
+    [0, dst_h - 1],
     [dst_w - 1, dst_h - 1],
-    [0, dst_h - 1]
+    [dst_w - 1, 0]
 ], dtype=np.float32)
 
-perspective_matrix = cv2.getPerspectiveTransform(src_quad, dst_quad)
-print(perspective_matrix)
 
-dst = cv2.warpPerspective(img, perspective_matrix, (dst_w, dst_h))
 
-preview = img.copy()
-for pt in src_quad.astype(int):
-    cv2.circle(preview, tuple(pt), 8, (0, 0, 255), -1)
+drag_src = [False, False, False, False]
 
-cv2.polylines(preview, [src_quad.astype(np.int32)], True, (0, 255, 0), 3)
 
-cv2.imshow("preview", preview)
+
+display = draw_roi(img, src_quad)
+
+
+
+cv2.imshow('img', display)
+cv2.setMouseCallback('img', on_mouse)
+
+
+print('네 꼭짓점을 드래그하여 영역을 맞추세요')
+print("Enter: 투시 전환")
+print("ESC: 종료")
+
+while True:
+    key = cv2.waitKey(0)
+    if key == 27:
+        cv2.destroyAllWindows()
+        raise SystemExit
+    elif key in (10, 13):
+        break
+
+prespective_matrix = cv2.getPerspectiveTransform(src_quad, dst_quad)
+dst = cv2.warpPerspective(img, prespective_matrix, (dst_w, dst_h), flags = cv2.INTER_CUBIC)
 cv2.imshow("perspective result", dst)
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+
+
+
+
+
+
+
+# ---------------------------------------------------------------------------------------
+# 내가
+# import cv2
+# import numpy as np
+
+# img = cv2.imread("./images/namecard.jpg")
+
+# # 변환된 크기를 이렇게 할거다
+# dst_w = 600
+# dst_h = 400
+
+# # 네 점의 순서는 왼쪽 위 > 오른쪽 위 > 오른쪽 아래 > 왼쪽 아래
+# src_quad = np.array([
+#     [16, 16],    
+#     [700, 16],   
+#     [700, 843],  
+#     [16, 843]     
+# ], dtype=np.float32)
+
+# dst_quad = np.array([
+#     [0, 0],
+#     [dst_w - 1, 0],
+#     [dst_w - 1, dst_h - 1],
+#     [0, dst_h - 1]
+# ], dtype=np.float32)
+
+# perspective_matrix = cv2.getPerspectiveTransform(src_quad, dst_quad)
+# print(perspective_matrix)
+
+# dst = cv2.warpPerspective(img, perspective_matrix, (dst_w, dst_h))
+
+# preview = img.copy()
+# for pt in src_quad.astype(int):
+#     cv2.circle(preview, tuple(pt), 8, (0, 0, 255), -1)
+
+# cv2.polylines(preview, [src_quad.astype(np.int32)], True, (0, 255, 0), 3)
+
+# cv2.imshow("preview", preview)
+# cv2.imshow("perspective result", dst)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
